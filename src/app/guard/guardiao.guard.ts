91679d5f06ace5cd2f3382fbc602bd1ee69693cf
faustus-gomes <faustus.gomes@gmail.com>
2026-03-08 22:44:15 -0300

    19.10- Requisição Síncrona de Possui Acesso

--- src/app/guard/guardiao.guard.ts
+++ src/app/guard/guardiao.guard.ts
@@ -1,14 +1,26 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { LoginService } from '../services/login.service';
+import { environment } from 'src/environments/environment';
 
 export const guardiaoGuard: CanActivateFn = (route, state) => {
 
   var username =  localStorage.getItem('username');
   var roles = route.data;
 
-  console.info('Username: '+ username);
-  console.info(roles);
+  //console.info('Username: '+ username);
+  //console.info(JSON.parse(JSON.stringify(roles)).role.toString());
+  var role = JSON.parse(JSON.stringify(roles)).role.toString();
+  var autorization = '' + localStorage.getItem('Authorization');
+
+  // Montar requisição sincrona
+  var request = new XMLHttpRequest();
+  request.open("GET", environment.urlApiLocal + 'possui acesso/' + username + '/'+ role, false);
+  request.setRequestHeader('Authorization', autorization);
+  request.send();
+
+  var possuiAcessoRetorno = request.responseText;
+  console.info('possuiAcessoRetorno : ' + possuiAcessoRetorno);
 
   return inject(LoginService).usuarioLogado();
 };
