0c9b9dd3529d8c2cf2720d5cbf1b85314c4ed544
faustus-gomes <faustus.gomes@gmail.com>
2026-06-12 18:12:43 -0300

    Ajuste visual Home

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+
+   // Métodos para navegação dos botões
+    acessarAdminSI() {
+        console.log('Navegando para Admin S.I');
+        // this.router.navigate(['/admin-si']);
+    }
+
+    acessarIntranet() {
+        console.log('Navegando para Intranet');
+        // this.router.navigate(['/intranet']);
+    }
+
+    acessarPortalSite() {
+        console.log('Navegando para Portal-Site');
+        // this.router.navigate(['/portal-site']);
+    }
+
+    // Métodos para o dashboard
+    verDetalhesPagar() {
+        console.log('Ver detalhes de contas a pagar');
+        // Implementar navegação ou modal
+        // this.router.navigate(['/contas-pagar']);
+    }
+
+    verDetalhesReceber() {
+        console.log('Ver detalhes de contas a receber');
+        // Implementar navegação ou modal
+        // this.router.navigate(['/contas-receber']);
+    }
+
 }
